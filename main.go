5bb3600801015bf604be7850fd8057280bf95ed9
matthias fifi <fifi.matthias@protonmail.com>
2023-12-23 22:52:41 +0100

    add form id

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"github.com/WWanderer/afclone/config/db"
 	"github.com/WWanderer/afclone/models/common"
 	"github.com/WWanderer/afclone/models/form1"
+	"github.com/google/uuid"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/session"
 	"github.com/gofiber/template/html/v2"
 )
 
@@ -27,8 +29,7 @@ func main() {
 	// translations := translations.GetTranslations()
 
 	db.ConnectDB()
-	// sessionStore := session.New()
-
+	sessionStore := session.New()
 
 	engine := html.New("resources/views", ".html")
 	engine.Reload(true)
@@ -40,33 +41,47 @@ func main() {
 	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{})
+		return c.Render("index", nil)
 	})
 
-	app.Get("form1/context", func(c *fiber.Ctx) error {
+	app.Get("/form1", func(c *fiber.Ctx) error {
+		sess, err := sessionStore.Get(c)
+		if err != nil {
+			log.Panic(err)
+		}
+		if err := sess.Save(); err != nil {
+			log.Panic(err)
+		}
+		formid := uuid.New()
+		return c.Redirect("/form1/" + formid.String() + "/context")
+	})
+
+	app.Get("form1/:formid/context", func(c *fiber.Ctx) error {
 		form1Context := form1.Context{} // TODO or load from db
 		// TODO inputs to be loaded from config file
 		return c.Render("form1/context", fiber.Map{"context": form1Context,
+			"formid": c.Params("formid"),
 			"durations":        []Duration{{Value: 1}, {Value: 2}, {Value: 3}},
 			"nationalAgencies": []Ccm2DisplayField{{DisplayValue: "BE01", Ccm2: 100}, {DisplayValue: "FR01", Ccm2: 101}, {DisplayValue: "DE01", Ccm2: 103}},
 			"languages":        []Ccm2DisplayField{{DisplayValue: "EN", Ccm2: 200}, {DisplayValue: "FR", Ccm2: 201}, {DisplayValue: "DE", Ccm2: 202}},
 		})
 	})
 
-	app.Patch("form1/context", func(c *fiber.Ctx) error {
+	app.Patch("form1/:formid/context", func(c *fiber.Ctx) error {
 		postedContext := new(form1.ContextDTO)
 		if err := c.BodyParser(postedContext); err != nil {
 			log.Println(err)
 			return err
 		}
-		formContext := postedContext.Map() 
-		
+		formContext := postedContext.Map()
+
 		applicationsCollection := db.Instance.DB.Collection("applications")
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 		applicationsCollection.InsertOne(ctx, formContext)
 
 		return c.Render("form1/context", fiber.Map{"context": formContext,
+			"formid":           c.Params("formid"),
 			"durations":        []Duration{{Value: 1}, {Value: 2}, {Value: 3}},
 			"nationalAgencies": []Ccm2DisplayField{{DisplayValue: "BE01", Ccm2: 100}, {DisplayValue: "FR01", Ccm2: 101}, {DisplayValue: "DE01", Ccm2: 103}},
 			"languages":        []Ccm2DisplayField{{DisplayValue: "EN", Ccm2: 200}, {DisplayValue: "FR", Ccm2: 201}, {DisplayValue: "DE", Ccm2: 202}},
